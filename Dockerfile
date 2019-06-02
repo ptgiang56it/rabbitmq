4a1d43c107518af79e4a18962faf71364204f619
Kevin Musselman <kmussel@gmail.com>
2019-06-02 13:14:34 -0400

    setup rabbit after started

--- Dockerfile
+++ Dockerfile
@@ -6,16 +6,17 @@ EXPOSE 15674
 
 WORKDIR /opt/rabbitmq
 
-COPY plugins/* plugins/
-RUN rabbitmq-plugins enable rabbitmq_web_stomp
-RUN rabbitmq-plugins enable rabbitmq_auth_backend_http
-RUN rabbitmq-plugins enable rabbitmq_auth_backend_ip_range
 
 COPY rabbitmq.conf /etc/rabbitmq/
 COPY advanced.config /etc/rabbitmq/
 COPY rabbitmq-env.conf /etc/rabbitmq/
 
-# RUN rabbitmqctl add_user rabbit rabbit
-# RUN rabbitmqctl set_user_tags rabbit administrator
-# RUN rabbitmqctl set_permissions -p / rabbit ".*" ".*" ".*"
+COPY plugins/* plugins/
 
+COPY setup_rabbit.sh .
+RUN chmod 777 setup_rabbit.sh
+
+# CMD ["/opt/rabbitmq/setup_rabbit.sh"]
+# RUN rabbitmq-plugins enable rabbitmq_web_stomp
+# RUN rabbitmq-plugins enable rabbitmq_auth_backend_http
+# RUN rabbitmq-plugins enable rabbitmq_auth_backend_ip_range
---
+++ setup_rabbit.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+rabbitmqctl wait $RABBITMQ_PID_FILE
+rabbitmqctl add_vhost users
+rabbitmqctl set_permissions --vhost users $RABBITMQ_DEFAULT_USER ".*" ".*" ".*"
+
+rabbitmqadmin declare exchange --user=$RABBITMQ_DEFAULT_USER --password=$RABBITMQ_DEFAULT_PASS name=DLX type=direct durable=true
+rabbitmqadmin declare queue --user=$RABBITMQ_DEFAULT_USER --password=$RABBITMQ_DEFAULT_PASS name=deadletters durable=true
+rabbitmqadmin declare binding --user=$RABBITMQ_DEFAULT_USER --password=$RABBITMQ_DEFAULT_PASS source=DLX destination=deadletters routing_key="#"
+
+rabbitmqadmin declare queue --user=$RABBITMQ_DEFAULT_USER --password=$RABBITMQ_DEFAULT_PASS name=svc.slice_status durable=true 'arguments={"x-dead-letter-exchange":"DLX"}'
+rabbitmqadmin declare binding --user=$RABBITMQ_DEFAULT_USER --password=$RABBITMQ_DEFAULT_PASS source=amq.topic destination=svc.slice_status routing_key="slice.status.#"
+
+rabbitmqadmin declare queue --user=$RABBITMQ_DEFAULT_USER --password=$RABBITMQ_DEFAULT_PASS name=svc.process_files durable=true 'arguments={"x-dead-letter-exchange":"DLX"}'
+rabbitmqadmin declare binding --user=$RABBITMQ_DEFAULT_USER --password=$RABBITMQ_DEFAULT_PASS source=amq.topic destination=svc.process_files routing_key="files.#"
+
+rabbitmqadmin declare queue --user=$RABBITMQ_DEFAULT_USER --password=$RABBITMQ_DEFAULT_PASS name=svc.slice durable=true 'arguments={"x-dead-letter-exchange":"DLX"}'
+rabbitmqadmin declare binding --user=$RABBITMQ_DEFAULT_USER --password=$RABBITMQ_DEFAULT_PASS source=amq.headers destination=svc.slice 'arguments={"x-match":"all", "service":"slicer", "slicer":"slic3r"}'
